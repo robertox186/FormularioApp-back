abfe590c916bcb9059c2794ab60d95f74a7fe3f9
Roberto Acosta <robetorobatox186@gmail.com>
2021-04-25 15:57:31 -0400

    d

--- server/controllers/properties.js
+++ server/controllers/properties.js
@@ -1,14 +1,14 @@
 module.exports={
 
-    insertUser:"insert into  usuario (username,email,contraseña,genero,admin,edad) values($1,$2,$3,$4,false,$5) returning *",
-    login:"select * from usuario where email=$1",
+    insertUser:"insert into  user_1 (username,email,contrasena,genero,admin,edad) values($1,$2,$3,$4,false,$5) returning *",
+    login:"select * from user_1 where email=$1",
     update:"update users  set informacion=$1 where email=$2",
 
     insertMenu:"insert into menu (titulo,parent_menu) values ($1,$2) returning *",
 
     select_all_menu:"select * from menu",
     
-     delete_menu:"delete from meno where id_menu=$1",
+     delete_menu:"delete from menu where id_menu=$1",
      insertForm:"insert into formulario (title,descripcion,id_menu) values ($1,$2,$3)",
 
      selectForm:"select  * from formulario where id_formulario=$1",
